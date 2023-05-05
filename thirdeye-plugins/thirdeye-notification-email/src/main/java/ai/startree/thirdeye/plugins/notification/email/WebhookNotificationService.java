0928dabcf373d3d3038f36e10bae62beac166f00
Yin Cao (yincao2) <yincao2@cisco.com>
2023-05-05 13:18:46 +0800

    add log json

--- thirdeye-plugins/thirdeye-notification-email/src/main/java/ai/startree/thirdeye/plugins/notification/email/WebhookNotificationService.java
+++ thirdeye-plugins/thirdeye-notification-email/src/main/java/ai/startree/thirdeye/plugins/notification/email/WebhookNotificationService.java
@@ -20,24 +20,28 @@ public class WebhookNotificationService implements NotificationService {
     @Override
     public void notify(NotificationPayloadApi api) throws ThirdEyeException {
         LOG.info("webhook success");
-        Map<String, String> headers = new HashMap<>();
-        headers.put("Content-Type", "application/json");
-        headers.put("Authorization", "Y2Q0YWQ1OGEtYTY2NS00ZjU0LWIzNGItODY3YzY5NjdlYTFkZmVhZThiMjAtZDcx_PF84_1eb65fdf-9643-417f-9974-ad72cae0e10f");
         Gson gson = new Gson();
-        String metrics = gson.toJson(api.getReport());
-        JSONObject jsonObject = new JSONObject();
-        jsonObject.put("roomId", "Y2lzY29zcGFyazovL3VzL1JPT00vMDFjZWRiZTAtZTNmNS0xMWVkLTkzNzQtNzlkNTc0ZTI1OTY3");
-        jsonObject.put("markdown", metrics);
-        try {
-            HttpResponse<JsonNode> jsonNodeHttpResponse = Unirest.post("")
-                    .headers(headers)
-                    .body(jsonObject.toString())
-                    .asJson();
-        } catch (Exception e) {
-            LOG.info("NotificationPayloadApi {}", api.toString());
+        LOG.info("NotificationPayloadApi:{}", gson.toJson(api));
 
-            LOG.error("webhook error", e);
-        }
+
+//        Map<String, String> headers = new HashMap<>();
+//        headers.put("Content-Type", "application/json");
+//        headers.put("Authorization", "Y2Q0YWQ1OGEtYTY2NS00ZjU0LWIzNGItODY3YzY5NjdlYTFkZmVhZThiMjAtZDcx_PF84_1eb65fdf-9643-417f-9974-ad72cae0e10f");
+//        Gson gson = new Gson();
+//        String metrics = gson.toJson(api.getReport());
+//        JSONObject jsonObject = new JSONObject();
+//        jsonObject.put("roomId", "Y2lzY29zcGFyazovL3VzL1JPT00vMDFjZWRiZTAtZTNmNS0xMWVkLTkzNzQtNzlkNTc0ZTI1OTY3");
+//        jsonObject.put("markdown", metrics);
+//        try {
+//            HttpResponse<JsonNode> jsonNodeHttpResponse = Unirest.post("")
+//                    .headers(headers)
+//                    .body(jsonObject.toString())
+//                    .asJson();
+//        } catch (Exception e) {
+//            LOG.info("NotificationPayloadApi {}", api.toString());
+//
+//            LOG.error("webhook error", e);
+//        }
 
 
     }
